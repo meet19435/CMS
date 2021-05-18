9975f56c90181390a303bc82c270a21e02829c3c
meet19435 <meet19435@iiitd.ac.in>
2021-05-19 00:24:15 +0530

    Added the App file

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask, request , render_template,redirect,url_for,flash
+import os
+import mysql.connector as sqlConnect
+import matplotlib.pyplot as plt
+from datetime import date,datetime
