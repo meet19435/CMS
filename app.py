fac63ea5136646a5194bda93bc0cce3cc0d2057b
Sameer Kumar <sameer19330@iiitd.ac.in>
2021-05-19 01:51:01 +0530

    Project inauguration login page connected thorugh flask

--- app.py
+++ app.py
@@ -3,3 +3,17 @@ import os
 import mysql.connector as sqlConnect
 import matplotlib.pyplot as plt
 from datetime import date,datetime
+from form import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		email = request.form.get("email")
+	return render_template('login.html', title = 'login')
+
+if __name__ == '__main__':
+	app.run(debug = True)
+
+
