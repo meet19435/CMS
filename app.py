c9026fd003b2a04123d54758cbb30f8d644438e9
meet19435 <meet19435@iiitd.ac.in>
2021-05-20 20:55:46 +0530

    added news to home page

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 from datetime import date,datetime
 ##from form import RegistrationForm, LoginForm
 from database import *
+from news import run
+
 
 app = Flask(__name__)
+
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -18,7 +22,13 @@ def login():
 @app.route('/home')
 @app.route('/')
 def home():
-	return render_template('home.html', title = 'Home - CMS')
+	data_news = run(5)
+	# test_news1 = data_news[0]
+	# test_news2 = data_news[1]
+	# test_news3 = data_news[2]
+	# test_news4 = data_news[3]
+	# test_news5 = data_news[4]
+	return render_template('home.html', title = 'Home - CMS', news =data_news)
 
 @app.route('/about')
 def about():
